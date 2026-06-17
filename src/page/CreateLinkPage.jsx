84d49df0c5522357798d15272b214ffb567d90b4
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-17 21:50:25 +0700

    feat: add create link page

---
+++ src/page/CreateLinkPage.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+import CreateLink from '../components/dashboard/CreateLink';
+import Footer from '../components/Footer';
+function CreateLinkPage() {
+  return (
+    <>
+      <Navbar />
+      <CreateLink />
+      <Footer />
+    </>
+  );
+}
+
+export default CreateLinkPage;
