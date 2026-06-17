4340e71259678997ffbff2fa1cd0c8cf57b725f7
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-17 21:50:14 +0700

    feat: add home page

---
+++ src/page/HomePage.jsx
@@ -0,0 +1,17 @@
+import Footer from '../components/Footer';
+import Descriptions from '../components/home/Descriptions';
+import Features from '../components/home/Features';
+import Hero from '../components/home/Hero';
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Descriptions />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
