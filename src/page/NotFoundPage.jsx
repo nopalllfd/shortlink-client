70e552a2743c4bdbdb080d5adcedd42b0bff7969
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-17 21:50:50 +0700

    feat: add notfound page

---
+++ src/page/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import Footer from '../components/Footer';
+import NotFound from '../components/notfound/Notfound';
+
+function NotFoundPage() {
+  return (
+    <>
+      <NotFound />
+      <Footer />
+    </>
+  );
+}
+
+export default NotFoundPage;
