37f97331e22d4a7042a333b5c2a4dd67c3095cb1
nopalllfd <Naufalfadhilah689@gmail.com>
2026-06-17 21:50:34 +0700

    feat: add dashboard page

---
+++ src/page/DashboardPage.jsx
@@ -0,0 +1,15 @@
+import MyLinks from '../components/dashboard/MyLinks';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+function DashboardPage() {
+  return (
+    <>
+      <Navbar />
+      <MyLinks />
+      <Footer />
+    </>
+  );
+}
+
+export default DashboardPage;
